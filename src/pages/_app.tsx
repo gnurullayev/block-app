66eb88d8496ece60258e8e28a08188cf6f9938e4
gnurullayev <nurullayevgayrat73@gmail.com>
2023-04-24 14:39:19 +0500

    add layout component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,39 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import 'src/styles/globals.css'
+import type { AppProps } from 'next/app'
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline  from '@mui/material/CssBaseline';
+import { CacheProvider,EmotionCache } from '@emotion/react';
+import theme from 'src/helpers/theme';
+import createEmotionCache from 'src/helpers/createEmotionCatch';
+import { useEffect } from 'react';
+import Head from 'next/head';
+
+const clientSideEmotionCache = createEmotionCache();
+ 
+export interface MyAppProps extends AppProps {
+  emotionCache?:EmotionCache;
+}
+export default function App(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps} = props
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles?.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
 }
